a045ab54bcaa201e37cbf1f0c325c63ae4bace68
Lu Chen <blacksea.chen@hotmail.com>
2017-05-10 18:17:07 +0800

    add functions of adding and deleting tasks

--- es6-react-sample/app/src/components/Todo/index.jsx
+++ es6-react-sample/app/src/components/Todo/index.jsx
@@ -7,15 +7,32 @@ const cx = classNames.bind(scss);
 class Todo extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {nextIndex:0, tasks: []};
+  }
+
+  deleteTask(taskId) {
+    let originTasks = this.state.tasks;
+    this.setState({tasks: originTasks.filter((task) => { return task !== taskId})}, () => {console.log('hello')});
+  }
+
+  addTask() {
+    let newTasks = this.state.tasks;
+    let index = this.state.nextIndex;
+    newTasks.push(index + 1);
+    this.setState({nextIndex:index + 1, tasks: newTasks}, null);
   }
 
   render () {
     return (
       <div className={ cx('todo-app') }>
         <h1>Todo List</h1>
+        <button onClick={() => this.addTask()}>add</button>
         <ul>
-          <li>item1</li>
-          <li>item2</li>
+          {
+            this.state.tasks.map((task) => {
+              return <li key={task}>item{task}<button onClick={() => this.deleteTask(task)}>delete</button></li>
+            })
+          }
         </ul>
       </div>
     );
